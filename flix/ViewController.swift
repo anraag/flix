4656db972c0d272a4feb3e6c8577516e3e89314c
anraag <anurag.yadav@bison.howard.edu>
2018-02-19 21:10:32 -0500

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Anurag Kumar Yadav on 2/19/18.
+//  Copyright © 2018 Anurag Kumar Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
