9e940c1df3f011f358cef84fb93a4b568ee9b9bf
anraag <anurag.yadav@bison.howard.edu>
2018-02-25 16:42:52 -0500

    added collection view

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Anurag Kumar Yadav on 2/25/18.
+//  Copyright © 2018 Anurag Kumar Yadav. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
